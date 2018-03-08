26c6bc44475e1696a0d5d4320344704d7351ca2b
Peter Gorgone <peter@peltronic.com>
2018-03-08 09:33:52 -0800

    update mark

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -25,7 +25,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-<!-- MARK RELEASE 20180209-a -->
+<!-- MARK RELEASE: release.api_updates--20180308  -->
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
