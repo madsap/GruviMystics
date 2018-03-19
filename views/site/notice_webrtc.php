ebe89f397786f67d598ffd3a75249bfd5a73a01a
Michael D. Johnson <michael.d.johnson1@charter.com>
2018-03-19 14:50:40 -0600

    Styled webrtc page. Still missing IOS link

--- views/site/notice_webrtc.php
+++ views/site/notice_webrtc.php
@@ -8,17 +8,44 @@ use \yii\helpers\Html;
 
 $this->title = 'Notice: WebRTC';
 ?>
-<div class="page-wrapper col-xs-12">
-    <div class="page-container panel panel-default">
-        <div class="panel-body text-violet">
-            <h4>foo bar foo </h4>
-            <h1 class="text-bold text-pink">Bar Foo Bar</h1>
-            <div class="row">
-                <div class="col-md-6 col-sm-6"></div>
-                <div class="col-md-6 col-sm-6">
-                    Lorum Ipsum...
-                </div>
-            </div>
+<div class="col-xs-12 non-rtc">
+    <div class="row gPanel-header">
+        <div class="col-xs-12 text-center">
+            ATTENTION: You are using a browser that is not currently supported.
+        </div>
+        <div class="col-xs-12 text-center">
+            Please use one of the following browsers or Download the app!
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-offset-1 col-md-3 col-xs-offset-3 col-xs-5 gPanel">
+            <div>Browsers that support WebRTC:</div>
+            <ul>
+                <li><em>Mac:</em>
+                    <ul>
+                        <li>FireFox</li>
+                        <li>Chrome</li>
+                    </ul>
+                </li>
+            </ul>
+            <ul>
+                <li><em>PC:</em>
+                    <ul>
+                        <li>Chrome</li>
+                        <li>Explorer</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-offset-1 col-md-2 col-xs-offset-3 col-xs-5 gPanel text-center">
+            <div><em>iOS:</em></div>
+            <div>App Store</div>
+            <a href="#" class="logo"><img src="/images/apple.png" alt=""></a>
+        </div>
+        <div class="col-md-offset-1 col-md-2 col-xs-offset-3 col-xs-5 gPanel text-center">
+            <div><em>Android:</em></div>
+            <div>Play Store</div>
+            <a href="https://play.google.com/store/apps/details?id=com.gruvimystics" class="logo"><img src="/images/play.png" alt=""></a>
         </div>
     </div>
 </div>
